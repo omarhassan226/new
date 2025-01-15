03cc50bad23096db977ef18b26ff718dbf5ec0ae
omarhassan226 <oh.22697@gmail.com>
2025-01-15 21:50:36 +0200

    Fixing jobs tabs bug

--- src/app/modules/jobs/components/red-section/red-section.component.ts
+++ src/app/modules/jobs/components/red-section/red-section.component.ts
@@ -8,40 +8,7 @@ import { AfterViewInit, ElementRef, ViewChild } from '@angular/core';
   templateUrl: './red-section.component.html',
   styleUrl: './red-section.component.css',
 })
-export class RedSectionComponent implements AfterViewInit {
+export class RedSectionComponent {
   @Input() activeTabs!: () => void;
-
-  @ViewChild('tabs') tabs: ElementRef | undefined;
-  @ViewChild('tabHeader') tabHeader: ElementRef | undefined;
-  @ViewChild('tabBody') tabBody: ElementRef | undefined;
-  @ViewChild('tabIndicator') tabIndicator: ElementRef | undefined;
-
-  ngAfterViewInit(): void {
-    // Ensure the DOM elements are available
-    if (this.tabs && this.tabHeader && this.tabBody && this.tabIndicator) {
-      let tabHeaderElements =
-        this.tabHeader.nativeElement.querySelectorAll('div');
-      let tabBodyElements = this.tabBody.nativeElement.querySelectorAll('div');
-
-      // Adding click event listeners to each tab header element
-      tabHeaderElements.forEach((element: HTMLElement, index: number) => {
-        element.addEventListener('click', () => {
-          // Remove active class from all tab headers and bodies
-          this.tabHeader?.nativeElement
-            .querySelector('.active')
-            ?.classList.remove('active');
-          this.tabBody?.nativeElement
-            .querySelector('.active')
-            ?.classList.remove('active');
-
-          element.classList.add('active');
-          tabBodyElements[index].classList.add('active');
-
-          if (this.tabIndicator) {
-            this.tabIndicator.nativeElement.style.left = `${index * 25}%`;
-          }
-        });
-      });
-    }
-  }
+  tabIndex = 0;
 }
